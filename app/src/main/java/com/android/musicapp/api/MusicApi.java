d92be011e12d608b8707b35b0471c32753aa42e4
JackyLee0904 <Jacky.Li0904@gmail.com>
2025-09-07 20:26:39 +0800

    Update MusicApi.java

---
+++ app/src/main/java/com/android/musicapp/api/MusicApi.java
@@ -0,0 +1,13 @@
+package  com.android.musicapp.api;
+public  class MusicApi {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+
+}
